4cc5c7ab065240e57c4f77e4e8295196791d1b26
itiya <fragment.vertex@gmail.com>
2019-01-02 22:33:06 +0900

    [add] repositoryが外部のものをinstallする機構を追加

--- etc/init/ubuntu/init.sh
+++ etc/init/ubuntu/init.sh
@@ -1,5 +1,29 @@
 #!/bin/sh
 
+install_with_repository() {
+  cat ${SCRIPTPATH}/apps_with_repository | while true
+  do
+    read app_name
+    if [ -z "$app_name" ] ; then break ; fi
+    read repository
+    if [ -z "$repository" ] ; then break ; fi
+
+    sudo apt-add-repository ${repository}
+  done
+
+  sudo apt-get update
+
+  cat ${SCRIPTPATH}/apps_with_repository | while true
+  do
+    read app_name
+    if [ -z "$app_name" ] ; then break ; fi
+    read repository
+    if [ -z "$repository" ] ; then break ; fi
+
+    sudo apt-get install -y ${app_name}
+  done
+}
+
 install() {
   cat ${SCRIPTPATH}/apps | while read app_name;
   do
@@ -13,4 +37,5 @@ echo 'Ubuntu mode'
 
 SCRIPTPATH=$(dirname $0)
 
+install_with_repository
 install
