4b1a305f0547ce66a2a997649b7466a1d68aeb8e
itiya <fragment.vertex@gmail.com>
2019-01-02 19:17:07 +0900

    [add] ubuntuのapt-getによるインストールスクリプト追加

--- etc/init/ubuntu/init.sh
+++ etc/init/ubuntu/init.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
+install() {
+  cat ${SCRIPTPATH}/apps | while read app_name;
+  do
+    if [ line != '' ]; then
+      sudo apt-get install -y ${app_name}
+    fi
+  done
+}
+
 echo 'Ubuntu mode'
+
+SCRIPTPATH=$(dirname $0)
+
+install
