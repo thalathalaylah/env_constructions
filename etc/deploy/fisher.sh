14272a8a090ac6eb140133ef1e7206fab3d0caf4
itiya <fragment.vertex@gmail.com>
2019-01-15 23:45:15 +0900

    [test] どこでconfig.fishが変更されているのか？

--- etc/deploy/fisher.sh
+++ etc/deploy/fisher.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 if [ ! -e ~/.config/fish/functions/fisher.fish ]; then
+  cat ~/.config/fish/fishfile
   curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisherman
+  cat ~/.config/fish/fishfile
 fi
 fish -c "fisher"
+cat ~/.config/fish/fishfile
